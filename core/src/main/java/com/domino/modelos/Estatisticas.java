8b6e487fe04632b0d9f868c7504ce4a3ee95e8d3
danielcruz604 <dancruzsan6@gmail.com>
2026-05-03 18:14:11 -0300

    feat(model): adiciona modelo de estatísticas

---
+++ core/src/main/java/com/domino/modelos/Estatisticas.java
@@ -0,0 +1,49 @@
+package com.domino.modelos;
+
+public class Estatisticas {
+    int partidasJogadas;
+    int partidasGanhas;
+    int partidasPerdidas;
+    int erros;
+    int acertos;
+
+    public int getPartidasJogadas() {
+        return partidasJogadas;
+    }
+
+    public void setPartidasJogadas(int partidasJogadas) {
+        this.partidasJogadas = partidasJogadas;
+    }
+
+    public int getPartidasGanhas() {
+        return partidasGanhas;
+    }
+
+    public void setPartidasGanhas(int partidasGanhas) {
+        this.partidasGanhas = partidasGanhas;
+    }
+
+    public int getPartidasPerdidas() {
+        return partidasPerdidas;
+    }
+
+    public void setPartidasPerdidas(int partidasPerdidas) {
+        this.partidasPerdidas = partidasPerdidas;
+    }
+
+    public int getErros() {
+        return erros;
+    }
+
+    public void setErros(int erros) {
+        this.erros = erros;
+    }
+
+    public int getAcertos() {
+        return acertos;
+    }
+
+    public void setAcertos(int acertos) {
+        this.acertos = acertos;
+    }
+}
