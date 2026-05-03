1e293277788776379de07591f3b5021e77a0add2
danielcruz604 <dancruzsan6@gmail.com>
2026-05-03 18:00:27 -0300

    feat(db): implementa conexão com banco de dados

---
+++ core/src/main/java/com/domino/bd/ConnectionFactory.java
@@ -0,0 +1,42 @@
+package com.domino.bd;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class ConnectionFactory {
+
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+
+    public ConnectionFactory(){
+        try {
+            Dotenv dotenv = Dotenv.load();
+            String connectionString = dotenv.get("MONGO_URI");
+
+            // Configuração do cliente MongoDB
+            ConnectionString connString = new ConnectionString(connectionString);
+            MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connString)
+                .build();
+
+            //Criando o cliente
+            mongoClient = MongoClients.create(settings);
+
+            // Selecionando o banco de dados:
+            database = mongoClient.getDatabase(dotenv.get("DATABASE_NAME"));
+
+            System.out.println("Conexão estabelecida com sucesso ao MongoDB!");
+        } catch (Exception e) {
+            System.err.println("Erro ao conectar ao MongoDB: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public MongoDatabase getDatabase() {
+        return database;
+    }
+}
