23a5fc940f17d0aeaf72f995b65e411bbb3d3825
danielcruz604 <dancruzsan6@gmail.com>
2026-05-10 18:10:41 -0300

    feat(main): define tela de login como tela inicial

--- core/src/main/java/com/domino/Main.java
+++ core/src/main/java/com/domino/Main.java
@@ -7,6 +7,6 @@ import com.domino.telas.LoginScreen;
 public class Main extends Game {
     @Override
     public void create() {
-        this.setScreen(new GameScreen());
+        this.setScreen(new LoginScreen());
     }
 }
