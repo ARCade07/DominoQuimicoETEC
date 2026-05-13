a4a4b24a28c836874a0a5c4528d72a81936cabb7
blueprint0 <guilhermegois2007@gmail.com>
2026-05-13 17:19:52 -0300

    refactor: simplifica montagem de Peça

--- core/src/main/java/com/domino/rede/packets/PacketJogada.java
+++ core/src/main/java/com/domino/rede/packets/PacketJogada.java
@@ -1,6 +1,7 @@
 package com.domino.rede.packets;
 
 import com.domino.logica.Tipo;
+import com.domino.logica.Peca;
 
 public class PacketJogada {
     public String info1;
@@ -9,4 +10,16 @@ public class PacketJogada {
     public Tipo tipo2;
     public boolean noFinal;
     public int proximoAJogar;
+
+    public int rotacao;
+    public boolean isBucha;
+
+    public void copiarPeca(Peca p){
+        this.info1 = p.getInfo1();
+        this.info2 = p.getInfo2();
+        this.tipo1 = p.getTipo1();
+        this.tipo2 = p.getTipo2();
+        this.rotacao = p.getRotacao();
+        this.isBucha = p.isBucha();
+    }
 }
