862a188bf8b344fdec1d93d335934c96426cdf65
Arthur <aheracliocunha@gmail.com>
2026-05-25 18:26:00 -0300

    feat: cria PacketResultadoJogo

---
+++ core/src/main/java/com/domino/rede/packets/PacketResultadoJogo.java
@@ -0,0 +1,9 @@
+package com.domino.rede.packets;
+
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class PacketResultadoJogo {
+    public ArrayList<PacketResultadoJogador> resultadoFinal = new ArrayList<>();
+}
