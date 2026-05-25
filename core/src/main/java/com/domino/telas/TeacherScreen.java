b04119dc81d67bed0e5f857e76ebfad361efa5e6
ARCade07 <alexandre.rios.cazzaniga@gmail.com>
2026-05-25 09:15:08 -0300

    feat: cria esqueleto da tela de professor

---
+++ core/src/main/java/com/domino/telas/TeacherScreen.java
@@ -0,0 +1,61 @@
+package com.domino.telas;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.NinePatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+
+public class TeacherScreen implements Screen {
+    private Stage stage;
+    private static final float MULTIPLICADOR_HD = 3.0f;
+
+    private BitmapFont fonteNormal;
+    private BitmapFont fonteNegrito;
+
+    private Texture texture;
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(Color.valueOf("0D0202"));
+
+        // Atualiza as animações e desenha todos os atores que estiverem no palco
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // Atualiza a visualização se a janela mudar de tamanho
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override public void pause() {}
+    @Override public void resume() {}
+    @Override public void hide() {}
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+        if (fonteNormal != null) fonteNormal.dispose();
+        if (fonteNegrito != null) fonteNegrito.dispose();
+        if (texture != null) texture.dispose();
+    }
