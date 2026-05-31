78cb861722a1a8dafd8f09d66ab573843c512f65
Arthur <aheracliocunha@gmail.com>
2026-05-31 13:13:14 -0300

    feat: atualiza jogadores no lobby

--- core/src/main/java/com/domino/rede/Cliente.java
+++ core/src/main/java/com/domino/rede/Cliente.java
@@ -3,6 +3,7 @@ package com.domino.rede;
 import com.badlogic.gdx.Gdx;
 import com.domino.rede.packets.*;
 import com.domino.telas.GameScreen;
+import com.domino.telas.TelaLobby;
 import com.esotericsoftware.kryonet.Client;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
@@ -13,24 +14,16 @@ public class Cliente {
     // instancia o cliente
     Client cliente = new Client();
     private GameScreen gameScreen;
+    private TelaLobby telaLobby;
     private String enderecoIP;
     public boolean minhaVez = false;
+    private int quantidadeJogadoresConectados = -1;
 
-    public Cliente (GameScreen tela, String enderecoIP){
-        this.gameScreen = tela;
+    public Cliente (String enderecoIP){
         this.enderecoIP = enderecoIP;
 
         // inicia a thread do cliente
         cliente.start();
-
-        try{
-            cliente.connect(5000, enderecoIP, 54555, 54777);
-        } catch (IOException e) {
-            System.out.println("Não foi possível se conectar a esse servidor");
-            cliente.stop();
-            throw new RuntimeException(e);
-        }
-
 
         // registros das classe que serão serializadas utilizando o kryo
         Registro.registrarClasses(cliente.getKryo());
@@ -43,7 +36,7 @@ public class Cliente {
                     minhaVez = (jogada.proximoAJogar == conexao.getID());
 
                     // caso a jogada seja válida
-                    if(jogada.info1 != null){
+                    if(jogada.info1 != null && gameScreen != null){
                         // acessa a thread principal do lib e envia para ela a alteração no tabuleiro
                         Gdx.app.postRunnable(new Runnable() {
                             @Override
@@ -67,8 +60,55 @@ public class Cliente {
                         System.out.println("Jogador: " + resultadoJogador.idJogador + " | Pontuação: " + resultadoJogador.pontuacao);
                     }
                 }
+                if(objeto instanceof PacketEntrouJogador){
+                    System.out.println("PACKET RECEBIDO!");
+
+                    PacketEntrouJogador entrouJogador = (PacketEntrouJogador) objeto;
+
+                    quantidadeJogadoresConectados = entrouJogador.quantidadeJogadores;
+
+                    System.out.println("Quantidade: " + entrouJogador.quantidadeJogadores);
+
+                    if(telaLobby != null){
+
+                        final int quantidade = entrouJogador.quantidadeJogadores;
+
+                        Gdx.app.postRunnable(() -> {telaLobby.atualizaJogadoresNaTela(quantidade);});
+                    }
+                }
             }
         });
+
+        try{
+            cliente.connect(5000, enderecoIP, 54555, 54777);
+        } catch (IOException e) {
+            System.out.println("Não foi possível se conectar a esse servidor");
+            cliente.stop();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void setGameScreen(GameScreen telaJogo){
+        this.gameScreen = telaJogo;
+    }
+
+    public void setTelaLobby(TelaLobby telaLobby){
+        this.telaLobby = telaLobby;
+        System.out.println(
+            "setTelaLobby chamado"
+        );
+
+        // resolve a race condition
+        System.out.println("ultimaQuantidadeJogadores = " + quantidadeJogadoresConectados);
+        if (quantidadeJogadoresConectados != -1) {
+            final int quantidade = quantidadeJogadoresConectados;
+            Gdx.app.postRunnable(new Runnable() {
+                @Override
+                public void run() {
+                    telaLobby.atualizaJogadoresNaTela(quantidade);
+                }
+            });
+        }
     }
 
     public void enviarJogada(PacketJogada jogada){
