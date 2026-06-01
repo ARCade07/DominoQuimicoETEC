93ed8c105529cef7341af9f3aca0c2361410ad2a
Arthur <aheracliocunha@gmail.com>
2026-06-01 18:26:28 -0300

    refactor: altera packet para carregar a lista de jogadores

--- core/src/main/java/com/domino/rede/packets/PacketEntrouJogador.java
+++
@@ -1,5 +0,0 @@
-package com.domino.rede.packets;
-
-public class PacketEntrouJogador {
-    public int quantidadeJogadores;
-}
---
+++ core/src/main/java/com/domino/rede/packets/PacketLobby.java
@@ -0,0 +1,7 @@
+package com.domino.rede.packets;
+
+import java.util.List;
+
+public class PacketLobby {
+    public List<Integer> idJogadoresConectados;
+}
