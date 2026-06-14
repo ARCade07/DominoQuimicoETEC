b9f8d9b2e0c266fee0a8fd6d526aa2cd4303316a
danielcruz604 <dancruzsan6@gmail.com>
2026-06-14 17:46:41 -0300

    feat(model): implementa classe de sessão

---
+++ core/src/main/java/com/domino/modelos/Sessao.java
@@ -0,0 +1,15 @@
+package com.domino.modelos;
+
+public class Sessao {
+
+    private static Usuario usuarioAtual;
+
+    public static void setUsuario(Usuario usuario) {
+        usuarioAtual = usuario;
+    }
+
+    public static Usuario getUsuario() {
+        return usuarioAtual;
+    }
+
+}
