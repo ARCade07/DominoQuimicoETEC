5b50444cd5ab23286ccefa78baa85bb05db929f3
danielcruz604 <dancruzsan6@gmail.com>
2026-06-14 17:38:20 -0300

    refactor(model): altera o construtor de usuário

--- core/src/main/java/com/domino/modelos/Usuario.java
+++ core/src/main/java/com/domino/modelos/Usuario.java
@@ -21,11 +21,10 @@ public class Usuario {
     }
 
     // O próprio bd gerá o id
-    public Usuario(String nome, String email, String senha, String role) {
+    public Usuario(String nome, String email, String senha) {
         this.nome = nome;
         this.email = email;
         this.senha = senha;
-        this.role = role;
         this.estat = new Estatisticas();
     }
 
